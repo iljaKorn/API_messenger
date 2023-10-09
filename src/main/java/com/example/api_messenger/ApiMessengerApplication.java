ec1e7c4e44642315503ec5354bffdefaf5f44dcc
iljaKorn <kornilov.ilja@rambler.ru>
2023-10-09 22:59:28 +0300

    Initial commit

---
+++ src/main/java/com/example/api_messenger/ApiMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_messenger/ApiMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
